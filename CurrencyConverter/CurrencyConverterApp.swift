984e7f3147cb0de52fd3bed41a45ab139a88cc84
Micah Moore <micah.moore12@gmail.com>
2024-02-19 12:39:54 -0600

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Micah Moore on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
